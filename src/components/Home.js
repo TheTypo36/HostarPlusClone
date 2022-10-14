e6c7c7425f5dcd7a6a92d583b8d573ef971dfaf6
Ashish Raghuvanshi <thetypo36@gmail.com>
2022-10-14 14:35:59 +0530

    home and image slide at top of home page:

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,31 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers.js";
+const home = () => {
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+    </Container>
+  );
+};
 
-const home = () => {};
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px) !important;
+  padding: 0 calc(3.5vw + 5px) !important;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  display: block;
+  top: 72px;
+  &:before {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 export default home;
---
+++ src/components/Viewers.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const Viewers = () => {
+  return <div>Viewers</div>;
+};
+export default Viewers;
